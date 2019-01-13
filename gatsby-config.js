52c7f980b12d048a5cc101244339e6c7c07c40d9
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-13 10:05:23 +0300

    Add gatsby plugin for google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Montserrat+Alternates`,
+          `source sans pro\:400,500,600,700`, // you can also specify font weights and styles
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',

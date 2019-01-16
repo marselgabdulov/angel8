824f41fab1da066cfc31d32f654ecbc39032ecb1
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-16 13:46:12 +0300

    Add InstagramCard Component

---
+++ src/components/InstagramCard/InstagramCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './InstagramCard.css'
+
+const InstagramCard = props => (
+  <a
+    className="instagram-card"
+    href={props.hreference}
+    target="_blank"
+    rel="noopener noreferrer"
+    style={{
+      backgroundImage: `url(${props.imageUrl})`,
+    }}
+  />
+)
+
+export default InstagramCard

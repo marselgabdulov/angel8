5ad2f6a80801be275b65a1aa5d2342e71d38bc3a
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-16 15:01:25 +0300

    Add empty contacts page

---
+++ src/pages/contacts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import Layout from '../components/layout'
+
+import './contactsPage.css'
+
+const ContactsPage = () => (
+  <Layout>
+    <div className="contacts">
+      <h1>Work in progress</h1>
+      <AniLink to="/" cover>
+        index page
+      </AniLink>
+    </div>
+  </Layout>
+)
+
+export default ContactsPage

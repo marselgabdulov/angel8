eb4ab200b84e0cd9e1ebc13120141ca9a59e9c07
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-18 14:03:13 +0300

    Make ImageGallery images as background images

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -2,22 +2,26 @@ import React from 'react'
 import { Fade } from 'react-slideshow-image'
 import './ImageGallery.css'
 
-const fadeProperties = {
-  duration: 3500,
-  transitionDuration: 1000,
-  infinite: true,
-  indicators: false,
-  arrows: false,
+class ImageGallery extends React.Component {
+  render() {
+    const galleryImages = this.props.images
+    const fadeProperties = {
+      duration: 3500,
+      transitionDuration: 1000,
+      infinite: true,
+      indicators: false,
+      arrows: false,
+    }
+    return (
+      <div className="image-gallery">
+        <Fade {...fadeProperties}>
+          {galleryImages.map((img, index) => (
+            <div key={index} style={{ backgroundImage: `url(${img})` }} />
+          ))}
+        </Fade>
+      </div>
+    )
+  }
 }
 
-const MenuImages = props => (
-  <div className="image-gallery">
-    <Fade {...fadeProperties}>
-      {props.images.map((each, index) => (
-        <img key={index} style={{ width: '100%' }} src={each} alt={index} />
-      ))}
-    </Fade>
-  </div>
-)
-
-export default MenuImages
+export default ImageGallery

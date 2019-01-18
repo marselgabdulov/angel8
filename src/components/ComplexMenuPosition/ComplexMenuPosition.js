2f40b2f06e2fb8d9c67fae49e95d1af9205dd038
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-18 19:43:14 +0300

    Some changes in menu styles

--- src/components/ComplexMenuPosition/ComplexMenuPosition.js
+++ src/components/ComplexMenuPosition/ComplexMenuPosition.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "./ComplexMenuPosition.css";
+import React from 'react'
+import './ComplexMenuPosition.css'
 
 const ComplexMenuPosition = props => (
   <div className="complex-menu-position">
     <div className="complex-menu-position__name">{props.name} </div>
-    <div className="complex-menu-position__ingredients">
-      &nbsp;({props.ingredients})
+    <div className="complex-menu-position__description">
+      &nbsp;({props.description})
     </div>
 
     <span />
     <div className="complex-menu-position__price">{props.price} &#8381;</div>
   </div>
-);
+)
 
-export default ComplexMenuPosition;
+export default ComplexMenuPosition

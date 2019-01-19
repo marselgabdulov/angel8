d6f6396d088fda5275724184ba943efbcc5fb1b5
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-19 18:40:25 +0300

    Try solve requestAnimationFrame problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,39 @@ import asianFood from '../images/instagram/asian_food.jpg'
 import bread from '../images/instagram/bread.jpg'
 
 class IndexPage extends React.Component {
+  componentDidMount() {
+    ;(function() {
+      var lastTime = 0
+      var vendors = ['ms', 'moz', 'webkit', 'o']
+      for (
+        var x = 0;
+        x < vendors.length && !window.requestAnimationFrame;
+        ++x
+      ) {
+        window.requestAnimationFrame =
+          window[vendors[x] + 'RequestAnimationFrame']
+        window.cancelAnimationFrame =
+          window[vendors[x] + 'CancelAnimationFrame'] ||
+          window[vendors[x] + 'CancelRequestAnimationFrame']
+      }
+
+      if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+          var currTime = new Date().getTime()
+          var timeToCall = Math.max(0, 16 - (currTime - lastTime))
+          var id = window.setTimeout(function() {
+            callback(currTime + timeToCall)
+          }, timeToCall)
+          lastTime = currTime + timeToCall
+          return id
+        }
+
+      if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+          clearTimeout(id)
+        }
+    })()
+  }
   render() {
     const fadeProperties = {
       duration: 3500,

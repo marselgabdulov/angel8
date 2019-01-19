e029ba1499ab9f48d7e951278c984b2846f2fc2c
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-19 12:17:03 +0300

    Little changes in styles

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import './contactsPage.css'
 
@@ -7,29 +6,67 @@ const ContactsPage = props => (
   <Layout>
     <div className="contacts-page">
       <div
-        className="contacts-page__map"
-        style={{
-          backgroundImage: `url(${props.data.map.childImageSharp.fluid.src})`,
-        }}
-      />
-      <section className="contacts-page__info">
+        className="contacts-page__title"
+        data-aos="fade"
+        data-aos-delay="100"
+        data-aos-duration="1200"
+        data-aos-once="true"
+      >
         <h1>Контакты</h1>
+      </div>
+      <div
+        className="contacts-page__info"
+        data-aos="fade"
+        data-aos-delay="150"
+        data-aos-duration="1200"
+        data-aos-once="true"
+      >
         <div className="contacts-page__address">
           <span>г. Тула</span>
           <br />
           <span>пр-т Ленина 85, кор. 1, вход 5</span>
           <br />
-          <span>300041</span>
-          <br />
           <span>Ликерка Лофт</span>
+          <br />
+          <span>300041</span>
         </div>
-        <br />
-        <div className="contacts-page__phone-email">
+        <div className="contacts-page__phone">
           <span>8 (4872) 77-02-47</span>
           <br />
           <span>angel8@gmail.com</span>
         </div>
-        <br />
+
+        <div className="contacts-page__maps">
+          <span>
+            <a
+              href="https://yandex.ru/maps/15/tula/?ll=37.600468%2C54.175741&mode=search&sll=37.600073%2C54.176105&sspn=0.002913%2C0.001007&text=%D0%BF%D1%80.%20%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0%2C%2085%2F1%2C%20%D0%A2%D1%83%D0%BB%D0%B0%2C%20%D0%A2%D1%83%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB.%2C%20300041&z=16.6"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Яндекс Карты
+            </a>
+          </span>
+          <br />
+          <span>
+            <a
+              href="https://www.google.ru/maps/place/%D0%BF%D1%80.+%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0,+85%2F1,+%D0%A2%D1%83%D0%BB%D0%B0,+%D0%A2%D1%83%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB.,+300012/@54.1754978,37.5985736,18.75z/data=!4m5!3m4!1s0x41343ff33ff3f2dd:0x50f6950475b6b04d!8m2!3d54.1754618!4d37.5993346?hl=ru&authuser=0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Google Maps
+            </a>
+          </span>
+          <br />
+          <span>
+            <a
+              href="https://2gis.ru/tula/query/%D1%82%D1%83%D0%BB%D0%B0%2C%20%D0%BB%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%2C%2085%20%D0%BA1/geo/5067185236026722?queryState=center%2F37.599345%2C54.176138%2Fzoom%2F16"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              2gis
+            </a>
+          </span>
+        </div>
         <div className="contacts-page__working-time">
           <span> пн-чт / 12:00-24:00 /</span>
           <br />
@@ -37,31 +74,7 @@ const ContactsPage = props => (
           <br />
           <span>вс / 12:00-24:00 /</span>
         </div>
-        <br />
-        <div className="contacts-page__maps">
-          <div className="contacts-page__map-links">
-            <span>
-              <a
-                href="https://yandex.ru/maps/15/tula/?ll=37.600468%2C54.175741&mode=search&sll=37.600073%2C54.176105&sspn=0.002913%2C0.001007&text=%D0%BF%D1%80.%20%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0%2C%2085%2F1%2C%20%D0%A2%D1%83%D0%BB%D0%B0%2C%20%D0%A2%D1%83%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB.%2C%20300041&z=16.6"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Яндекс Карты
-              </a>
-            </span>
-            <br />
-            <span>
-              <a
-                href="https://www.google.ru/maps/place/%D0%BF%D1%80.+%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0,+85%2F1,+%D0%A2%D1%83%D0%BB%D0%B0,+%D0%A2%D1%83%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB.,+300012/@54.1754978,37.5985736,18.75z/data=!4m5!3m4!1s0x41343ff33ff3f2dd:0x50f6950475b6b04d!8m2!3d54.1754618!4d37.5993346?hl=ru&authuser=0"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Google Maps
-              </a>
-            </span>
-          </div>
-        </div>
-        <br />
+
         <div className="contacts-page__social">
           <a
             href="https://www.facebook.com/Angel-8-2123834887946466/"
@@ -79,27 +92,9 @@ const ContactsPage = props => (
             Ins
           </a>
         </div>
-      </section>
+      </div>
     </div>
   </Layout>
 )
-
-export const fluidContactsImage = graphql`
-  fragment fluidContactsImage on File {
-    childImageSharp {
-      fluid(maxWidth: 900) {
-        ...GatsbyImageSharpFluid
-      }
-    }
-  }
-`
-
-export const pageQuery = graphql`
-  query {
-    map: file(relativePath: { eq: "map.png" }) {
-      ...fluidContactsImage
-    }
-  }
-`
 
 export default ContactsPage

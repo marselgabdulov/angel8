227c06cf333dba33e7653c7013e1fd64354b7807
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-01-22 19:15:00 +0300

    Customize 404 pfge

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,34 @@
 import React from 'react'
-
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+
+import './404.css'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="four-zero-four">
+      <SEO title="404: Not found" />
+      <h1 className="four-zero-four__title">404</h1>
+      <p className="four-zero-four__description">
+        Вы зашли на страницу, которой не существует. Какая досада...
+      </p>
+      <br />
+      <br />
+      <div className="four-zero-four__link">
+        <AniLink
+          to="/"
+          cover
+          bg="#181818"
+          activeStyle={{
+            fontWeight: 'bold',
+            textDecoration: 'underline',
+          }}
+        >
+          На главную
+        </AniLink>
+      </div>
+    </div>
   </Layout>
 )
 

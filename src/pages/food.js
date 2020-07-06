9f12192fae286c61adefa855f69e13a25e00b71b
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-07-06 14:35:28 +0300

    Change menu pages

--- src/pages/food.js
+++ src/pages/food.js
@@ -17,19 +17,13 @@ class FoodPage extends React.Component {
       <>
         <SEO title="Меню Блюда" />
         <Layout>
-          <div className="wrapper">
-            <div className="menu-page">
-              <div className="menu-page__upper">
-                \&nbsp; \&nbsp; \&nbsp; \&nbsp; \&nbsp; \&nbsp; \&nbsp; \&nbsp;
-                \&nbsp; \&nbsp; \&nbsp; \&nbsp; \&nbsp; \&nbsp; \
-              </div>
-              <div className="menu-page__wrapper">
-                <section className="menu-page__section">
-                  <h1>Меню блюда</h1>
-                  <a href={menu} download>
-                    Скачать меню PDF
-                  </a>
-                  {/* <div className="menu-page__section-title">
+          <div className="menu-page">
+            <div className="menu-page__wrapper">
+              <h1>Блюда</h1>
+              <a href={menu} download>
+                Скачать меню PDF
+              </a>
+              {/* <div className="menu-page__section-title">
                     ПОЗДНИЕ ЗАВТРАКИ 12:00-15:00
                   </div>
                   <div className="menu-page__section-subtitle">Яйца & Хлеб</div>
@@ -40,64 +34,61 @@ class FoodPage extends React.Component {
                       price={position.price}
                     />
                   ))} */}
-                </section>
-                <section className="menu-page__section">
-                  <span className="menu-page__section-title">ЗАКУСКИ</span>
-                  {starters.map(position => (
-                    <SimpleMenuPosition
-                      key={position.id}
-                      name={position.name}
-                      price={position.price}
-                    />
-                  ))}
-                </section>
-                <section className="menu-page__section">
-                  <span className="menu-page__section-title">САЛАТЫ</span>
-                  {salads.map(position => (
-                    <SimpleMenuPosition
-                      key={position.id}
-                      name={position.name}
-                      price={position.price}
-                    />
-                  ))}
-                </section>
-                <section className="menu-page__section">
-                  <span className="menu-page__section-title">СУПЫ</span>
-                  {soups.map(position => (
-                    <SimpleMenuPosition
-                      key={position.id}
-                      name={position.name}
-                      price={position.price}
-                    />
-                  ))}
-                </section>
-                <section className="menu-page__section">
-                  <span className="menu-page__section-title">
-                    ГРИЛЬ & ГОРЯЧЕЕ
-                  </span>
-                  {hotAndGrill.map(position => (
-                    <SimpleMenuPosition
-                      key={position.id}
-                      name={position.name}
-                      price={position.price}
-                    />
-                  ))}
-                </section>
-                <section className="menu-page__section--last">
-                  <span className="menu-page__section-title">ДЕСЕРТЫ</span>
-                  {desserts.map(position => (
-                    <SimpleMenuPosition
-                      key={position.id}
-                      name={position.name}
-                      price={position.price}
-                    />
-                  ))}
-                </section>
-              </div>
+              <section className="menu-page__section">
+                <span className="menu-page__section-title">ЗАКУСКИ</span>
+                {starters.map(position => (
+                  <SimpleMenuPosition
+                    key={position.id}
+                    name={position.name}
+                    price={position.price}
+                  />
+                ))}
+              </section>
+              <section className="menu-page__section">
+                <span className="menu-page__section-title">САЛАТЫ</span>
+                {salads.map(position => (
+                  <SimpleMenuPosition
+                    key={position.id}
+                    name={position.name}
+                    price={position.price}
+                  />
+                ))}
+              </section>
+              <section className="menu-page__section">
+                <span className="menu-page__section-title">СУПЫ</span>
+                {soups.map(position => (
+                  <SimpleMenuPosition
+                    key={position.id}
+                    name={position.name}
+                    price={position.price}
+                  />
+                ))}
+              </section>
+              <section className="menu-page__section">
+                <span className="menu-page__section-title">
+                  ГРИЛЬ & ГОРЯЧЕЕ
+                </span>
+                {hotAndGrill.map(position => (
+                  <SimpleMenuPosition
+                    key={position.id}
+                    name={position.name}
+                    price={position.price}
+                  />
+                ))}
+              </section>
+              <section className="menu-page__section--last">
+                <span className="menu-page__section-title">ДЕСЕРТЫ</span>
+                {desserts.map(position => (
+                  <SimpleMenuPosition
+                    key={position.id}
+                    name={position.name}
+                    price={position.price}
+                  />
+                ))}
+              </section>
             </div>
           </div>
         </Layout>
-        <Footer />
       </>
     )
   }

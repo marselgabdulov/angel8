199909185216b8340676e2c4099d1453530dedb3
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-07-13 10:04:14 +0300

    Change Intro section styles

--- src/components/IntroSection/IntroSection.js
+++ src/components/IntroSection/IntroSection.js
@@ -36,31 +36,43 @@ function IntroImage({ image }) {
 function IntroSection({ introImage }) {
   return (
     <section id="intro">
-      <div className="intro__slogan">
-        <div className="intro__title">
-          <LogoDesk />
+      <IntroImage image={introImage} />
+      <div className="intro__logo">
+        <LogoDesk />
+      </div>
+      <div className="intro__info">
+        <h1 className="intro__title">Ресторан. Гриль. Бар.</h1>
+        <h4 className="intro__subtitle">Контакты</h4>
+        <div className="intro__contacts">
+          <span>Тула проспект Ленина 85</span>
+          <span>корпус 1 вход 5</span>
+          <a href="tel:+74872770247">+7 4872 77 02 47</a>
         </div>
-        <h4 className="intro__description">Ресторан. Гриль. Бар.</h4>
-      </div>
-      <div className="intro__address">
-        <span></span>
-        Тула проспект Ленина 85
-        <span>корпус 1 вход 5</span>
-        <br />
-        <a href="tel:+74872770247">+7 4872 77 02 47</a>
-        <br />
-      </div>
-      <div className="intro__working-time">
-        <p>Воскресенье - Четверг 12:00 - 24:00</p>
-        <p>Пятница и Суббота 12:00 - 02:00</p>
-      </div>
-      <div className="intro__menu-pdf">
+        <h4 className="intro__subtitle">Ждем Вас</h4>
+        <span>Воскресенье - Четверг 12:00 - 24:00</span>
+        <span>Пятница и Суббота 12:00 - 02:00</span>
         <a href={menuPDF} target="_blank" rel="noopener noreferrer">
           Меню PDF
         </a>
+        <div className="intro__social-mobile">
+          <h4 className="intro__subtitle">Соцсети</h4>
+          <a
+            href="https://www.instagram.com/angel_bar_grill/?hl=ru"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.facebook.com/Angel-8-2123834887946466/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+        </div>
       </div>
-      <IntroImage image={introImage} />
-      <div className="intro__social">
+      <div className="intro__social-desk">
         <a
           href="https://www.instagram.com/angel_bar_grill/?hl=ru"
           target="_blank"
